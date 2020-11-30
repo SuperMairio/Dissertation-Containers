003e9df518aa25722aa7fddb201bdf04119314f6
1320071 <1700231@uad.ac.uk>
2020-11-30 13:36:55 +0000

    Actually added python2 folder

---
+++ Python Containers/Python3/script.py
@@ -0,0 +1,138 @@
+### GPA CALCULATOR BY MAIRI MACLEOD ###
+#Designed for Abertay Uni's GPA system feel free to alter as you need x
+
+import os
+from datetime import date
+
+gradeDict = {"A+":4.5, "A":4, "B+":3.5, "B":3, "C+":2.5, "C":2, "D+":1.5, "D":1, "MF":0.5} 
+
+GRADES = os.getenv("GRADES")
+GPA = os.getenv("GPA")
+FILEPATH = os.getenv("PATH")
+
+#Removes lowest x amount of grades (for covid weighting changes)
+def RemoveLowest(currentGrades):
+    howMany = int(input("How many grades can be removed?\n(This assumes your lowest grades will already have been gotten) "))
+
+    if len(currentGrades) == 12:
+        currentGrades.sort()
+    
+        while howMany != 0:
+            currentGrades.pop(0)
+            howMany -= 1
+
+    return(currentGrades)
+
+#Calculates GPA with current grades and prints out the grade associated with it
+def CalculateGPA():
+    currentGrades = GetGrades()
+    howMany = len(GRADES)
+    gpa = 0
+    letterGrade = 0
+    i = len(gradeDict)
+    numberOfGrades = len(currentGrades)
+    gradeList = list(gradeDict.keys())
+
+    for grade in currentGrades:
+        gpa += grade
+
+    gpa = gpa/numberOfGrades
+
+    #iterates through the dictionary and lets me access just the values
+    for letter, value in gradeDict.items():
+        if gpa >= value:
+            letterGrade += 0.5
+            i -= 1 
+    
+    os.chdir(FILEPATH)
+    today = date.today()
+    with open("GradeHistory.txt", w) as file:
+        file.write("\nGPA on", today,":", gpa, currentGrades)
+
+    print("Your current GPA: ("+gradeList[i]+")",gpa)
+
+def GetGrades():
+    addGrades = input("Would you like to add more grades?(y/n) ")
+    currentGrades = GRADES
+
+    if addGrades == "y":
+        howMany = int(input("How many would you like to add? ")
+
+        while howMany != 1:
+            extras = input("Please add the next grade:")
+            currentGrades.append(extras)
+            howMany -= 1
+
+    return(currentGrades)
+
+def CalculateRequired(numHave):
+    toGet = int(input("How many modules do you have left? "))
+    goal = float(input("What are you aiming for? (goal GPA) "))
+
+    currentGrades = RemoveLowest(GetGrades())
+    totalGrades = toGet + len(currentGrades)
+    dictKeys = list(gradeDict.keys())
+    dictValues = list(gradeDict.values())
+
+    newGrades = []
+    currentSum = 0
+    newSum = 0
+    x = 0
+
+    print("Calculating....")
+
+    #This is some gross maths, I apologise
+    for grade in currentGrades:
+        currentSum += grade
+
+    #add values to the array
+    while x < toGet:
+        print(newGrades)
+        newGrades.append(0)
+        x += 1
+
+    y = 0
+
+    while (currentSum + newSum) < (goal * totalGrades):
+        print("adding 0.5s")
+        if newGrades[y] < 4.5:
+            newGrades[y] += 0.5
+            newSum += 0.5
+        else:
+            y += 1
+        print(newSum)
+
+    #if any grades are super low bump them to try and balance the grades
+    #this works because if a grade is super low you must have at least one 4.5
+    goes = 0
+    while goes < 3:
+        z = -1
+        for score in newGrades:
+            z += 1
+            if score <= 2:
+                newGrades[z] += 1
+                try:
+                    highest = newGrades.index(4.5)
+                    newGrades[highest] -= 1
+                except(ValueError):
+                    newGrades[z] -= 1
+        goes += 1
+
+    print("Your required grades are:")
+
+    for letterGrades in newGrades:
+        print(letterGrades,":", dictKeys[dictValues.index(letterGrades)])
+
+
+print("******************************\n*** Mairi's GPA Calculator ***\n******************************\n1: Get current GPA\n2: Calculate required grades\n3: Calculate potential GPA ")
+userInput = input("\nPlease pick an option: ") 
+
+if userInput == "1":
+    print("GPA:",GPA,"\nGrades:",GRADES)
+elif userInput == "2":
+    howMany = int(input("How many grades do you currently have? "))
+    CalculateRequired(howMany)
+elif userInput == "3":
+    CalculateGPA()
+else:
+    exit()
