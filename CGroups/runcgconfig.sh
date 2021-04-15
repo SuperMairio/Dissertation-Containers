eadec812077c8df7234faf6a3821c35f926ffa66
1320071 <1700231@uad.ac.uk>
2021-04-15 22:08:49 +0100

    commented code

--- CGroups/runcgconfig.sh
+++ CGroups/runcgconfig.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# Installing the required packages
 apt-get install libcgroup-dev cgroup-lite cgroup-tools cgroupfs-mount libcgroup1
+
+# Moving the config file to the correct folder
 mv  cgconf.config /etc/ && cd /etc
+
+# Enabling and starting the service
 systemctl enable cgconfig
 systemctl start cgconfig
-cgexec -g memory,cpuset:dckr docker -it --rm ubuntu
+
+# Create and run container with cgroups
+docker-compose up --rm exampleContainer
+cgexec -g memory,cpuset:dckr docker exec -it exampleContainer /bin/bash
